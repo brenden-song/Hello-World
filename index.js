c86187aa4a6c370e21c0cd09cdad838e53453d6c
Brenden Song <brendensong@Brenden-Songs-MacBook-Pro.local>
2019-07-27 15:47:52 -0700

    prepare for heroku

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
